4deab9cf21e07ed0625e39ba25708bc89b6af1fc
Joao Pedro Carneiro <jcarneir@usc.edu>
2017-01-15 12:33:56 -0800

    set to Destroy lines

---
+++ BallSpliceUnityNew/Assets/Script/BallMovement.cs
@@ -0,0 +1,145 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//using SQLiteDatabase;
+
+public class BallMovement : MonoBehaviour {
+
+    public GameObject[] balls;
+
+    public int number = 0;
+
+
+
+
+    // Use this for initialization
+    void Start () {
+        balls = GameObject.FindGameObjectsWithTag("Ball");
+
+        foreach (GameObject ball in balls)
+        {
+            ball.transform.localPosition = new Vector3(0, 0, 0);
+
+            float strength = 200f;
+            float x = strength * Random.Range(-1, 1);
+            float y = strength * Random.Range(-1, 1);
+            float z = 0f;
+            Vector3 forceDirection = new Vector3(x, y, z);
+
+            ball.GetComponent<Rigidbody>().AddForce(forceDirection, ForceMode.Force);
+            ball.GetComponent<Rigidbody>().AddTorque(forceDirection);
+        }
+
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        balls = GameObject.FindGameObjectsWithTag("Ball");
+
+        foreach (GameObject ball in balls)
+        {
+            float position = ball.transform.position.z; 
+            if (position != 0)
+            {
+                Vector3 zConstant = new Vector3(ball.transform.position.x, ball.transform.position.y, 0.00f);
+                ball.transform.position = zConstant;
+            }
+        }    
+
+	}
+    private void OnCollisionEnter(Collision collision)
+    {
+		// fix the bouncing, perhaps do not add speed and movement variation
+        number = number +1;
+       // Debug.Log(number);
+        foreach(GameObject ball in balls)
+        {
+
+            float xSpeed = ball.GetComponent<Rigidbody>().velocity.x;
+            float ySpeed = ball.GetComponent<Rigidbody>().velocity.y;
+            float zSpeed = ball.GetComponent<Rigidbody>().velocity.z;
+
+            float strength = 100f * number;
+            float x = xSpeed + 5 *number * Time.deltaTime;
+            if (x < 5f && x >0f)
+            {
+                x = x + 1f;
+            }else if (x <0f && x > -5f)
+            {
+                x = x - 1f;
+            }else if (x > 13f)
+            {
+                x = x - 1f;
+            }else if (x < -13f)
+            {
+                x = x + 1f;
+            }
+            else if (x > 25f)
+            {
+                x = x - 20f;
+            }
+            else if (x < -25)
+            {
+                x = x + 20f;
+            }
+            else
+            {
+                x = 5f;
+            }
+
+            float y = ySpeed + 5 * number * Time.deltaTime;
+            if (y < 5f && y > 0f)
+            {
+                y = y + 1;
+            }
+            else if (y < 0f && y > -5f)
+            {
+                y = y - 1;
+            }
+            else if (y > 13f)
+            {
+                y = y - 1;
+            }
+            else if (y < -13f)
+            {
+                y = y + 1;
+            }
+            else if (y > 25f)
+            {
+                y = y - 20f;
+            }
+            else if (y < -25)
+            {
+                y = y + 20f;
+            }
+            else
+            {
+                y = 5f;
+            }
+
+            float z = zSpeed;
+            Vector3 forceDirection = new Vector3(x, y, z);
+            Debug.Log(forceDirection);
+            if (xSpeed < ySpeed)
+            {
+                ball.GetComponent<Rigidbody>().AddForce(25f*(Random.Range(1, -1)), 0f, 0f);
+            }else
+            {
+                ball.GetComponent<Rigidbody>().AddForce(25f, 50f * (Random.Range(1, -1)), 0f);
+            }
+
+            ball.GetComponent<Rigidbody>().velocity = forceDirection;
+
+           // ball.GetComponent<Rigidbody>().AddForce(forceDirection, ForceMode.Force);
+           // ball.GetComponent<Rigidbody>().AddTorque(forceDirection);
+        }
+    }
+
+
+
+    void hitMovement() {
+
+    }
+
+}
